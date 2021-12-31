33d36d9058f299cc91d6c534bdc568f0f5081042
IlyaMeli <ilyameli93@gmail.com>
2021-12-31 13:56:34 +0200

    working progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React, { createContext, useEffect, useState } from "react";
 import "./App.css";
-
+import React from "./pages/Route";
+import AppContext from "./component/AppContext"
 function App() {
   return (
+    <AppContext.Provider value={"check"}>
     <div className="App">
-      <h1>Check</h1>
+      <Route />
     </div>
+    </AppContext.Provider>
   );
 }
 
---
+++ src/pages/Route.js
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+
+const Route = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Route
