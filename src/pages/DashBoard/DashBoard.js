6a7052a67cb688c574c3d98a7cc2a0a4dea53621
IlyaMeli <ilyameli93@gmail.com>
2022-01-03 23:48:40 +0200

    progress work

--- src/pages/DashBoard/DashBoard.js
+++ src/pages/DashBoard/DashBoard.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import AppContext from "../../component/AppContext/AppContext";
+import { Line } from "rc-progress";
 import "./dashboard.css";
 
 const DashBoard = () => {
@@ -18,7 +19,12 @@ const DashBoard = () => {
           Great Job.
         </div>
         <div className="dash-med-lvl">
-          Your Meditation Rank is: {appContext.user.medLevel}
+          {/* Your Meditation Rank is: {appContext.user.medLevel} */}
+          <Line
+            percent="95"
+            strokeWidth="3"
+            strokeColor="rgba(49, 144, 251, 0.8)"
+          />
         </div>
       </div>
     </div>
