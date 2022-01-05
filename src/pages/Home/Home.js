bcb6a745bf23b3e2c117625b30166a3beb778a09
IlyaMeli <ilyameli93@gmail.com>
2022-01-06 01:43:48 +0200

    live release

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,18 +1,15 @@
-import React, { useState, useEffect  } from "react";
+import React, { useState, useEffect } from "react";
 import TextTransition, { presets } from "react-text-transition";
 import "./home.css";
 
-
 const Home = () => {
-  const TEXTS = ["Free Your Mind", "Meditate Today", "Mordi"];
+  const TEXTS = ["Free Your Mind", "Meditate Today", "Quiet the soul"];
 
   const [index, setIndex] = useState(0);
   useEffect(() => {
     const intervalId = setInterval(() => setIndex((index) => index + 1), 2000);
     return () => clearTimeout(intervalId);
   }, []);
-  
-
 
   return (
     <>
